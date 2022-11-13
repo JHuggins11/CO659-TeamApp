cee70eedc9cb52cdbd5cac05826fc575135f3d2c
Izzy <isabelle_thorpe@outlook.com>
2022-11-13 15:39:13 +0000

    initial commit - asp core razor

---
+++ ASP-RAZOR-TeamApp/ASP-RAZOR-TeamApp/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP_RAZOR_TeamApp.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
