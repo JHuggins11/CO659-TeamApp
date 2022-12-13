137d8da7dae6181ae5fef36432bf36b355858730
Izzy <isabelle_thorpe@outlook.com>
2022-12-13 20:17:13 +0000

    updated models

---
+++ CO659-TeamApp/CO659-TeamApp/Data/Migrations/20221213201233_InitialCreate1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CO659_TeamApp.Data.Migrations
+{
+    public partial class InitialCreate1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
